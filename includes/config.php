732819725c8537b0a8d0ab34e0d92c3805e28fa6
Jusxx <diazzhen06@gmail.com>
2026-03-16 09:35:53 +0800

    Remove SMS API token from config

--- includes/config.php
+++ includes/config.php
@@ -12,9 +12,6 @@ $conn->set_charset("utf8mb4");
 
 
 
-// // PhilSMS v3 config
-// $philsms_api_token = "848|igzCQi9nD9BABZQCpznBr9wxbPI4cl8ZMdpiaXpi0c8d4cc1";
-// $philsms_sender_id = "PhilSMS"; 
 
 
 $env = parse_ini_file(__DIR__ . '/.env');
